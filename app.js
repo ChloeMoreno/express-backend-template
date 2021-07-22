e08db5a43e5cf7d5b39ac618bfdfc32a9fdeb5af
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-07-22 16:50:23 +0200

    added route for favorites

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 // const { routes } = require('./routes');
 const express = require('express');
+const connection = require('./db-config');
 const app = express();
+const cors = require('cors')
+app.use(cors());
 
 const port = process.env.PORT || 1234;
 
@@ -12,3 +15,55 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.get('/queens', (req,res)=> {
+  connection.query('SELECT * FROM queen', (err, result)=> {
+    if (err) {
+      res.status(500).send('error retrieving data from database');
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
+
+app.get('/favorites', (req, res)=> {
+  connection.query('SELECT * FROM queen WHERE is_favorite = true',(err, result) => {
+    if (err) {
+      res.status(500).send('error retrieving data from database');
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
+
+app.get('/queens/:id', (req,res)=> {
+  const queenId = req.params.id;
+  connection.query('SELECT * FROM queen WHERE id = ?',[queenId],
+     (err, result)=> {
+        if (err) {
+          res.status(500).send('error retrieving data from database');
+        } else {
+          if (result.length) res.json(result[0]);
+          else res.status(404).send('queen not found')
+    }
+  })
+})
+
+
+
+app.put('/queens/:id', (req, res) => {
+  const queenId = req.params.id;
+  const queenPropsToUpdate = req.body;
+  connection.query(
+    'UPDATE queen SET ? WHERE id = ?',
+    [queenPropsToUpdate, queenId],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('error updating a queen')
+      } else {
+        res.status(200).send('queen updated successfully')
+      }
+    }
+  )
+})
